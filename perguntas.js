e41d6cebc1b005b4649108d32b21594062fe9fae
@Kaio Henrique <0000107761634xsp@al.educacao.sp.gov.br>
2024-11-28 18:13:53 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Carro',
+    'Qual o carro da nissan que termina com 5 e começa com R ?',
+    'É o R35'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Russia ?'
+    'Moscou'
+)
+
+criaCartao(
+    'Máquinas',
+    'Pra que serve a bobina de ignição de um motor ?',
+    'A bobina de ignição é um componente que atua como transformador elétrico, convertendo a baixa voltagem da bateria – que geralmente é de apenas 12 volts – para os milhares de volts necessários para gerar a faísca. '
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI '
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto que é 13 x 14',
+    '182'
+)
+
+criaCartao(
+    'Carro',
+    'Qual o carro da Toyota que começa com s e termina com 4',
+    'Supra mk4'
+)
